1fc8ad5c7e4fa7ca558e0fc997b1a55fbc901595
Igor Tolmachov <me@igorek.dev>
2023-01-25 21:00:46 +0900

    Some improvments

--- game/engine/course.py
+++ game/engine/course.py
@@ -63,15 +63,80 @@ class Course:
         self.balls.append(ball)
         return ball
 
-    def update(self) -> None:
-        time = self.clock.tick(self.fps) / 1000
+    def update_impulse(self, ball: Ball) -> None:
+        for collided_ball in self.balls:
+            if (
+                ball is collided_ball
+                or (collided_ball.pos - ball.pos).length_squared() == 0
+                or not pg.sprite.collide_circle(collided_ball, ball)
+            ):
+                continue
 
-        for ball in self.balls:
-            friction = self.model.friction
+            b1 = collided_ball
+            b2 = ball
 
-            for zone in self.zones:
-                if not pg.sprite.collide_mask(zone, ball):
-                    continue
+            v1 = b1.velocity - (
+                ((2 * b2.mass) / (b1.mass + b2.mass))
+                * (
+                    ((b1.velocity - b2.velocity).dot(b1.pos - b2.pos))
+                    / (b1.pos - b2.pos).length_squared()
+                )
+                * (b1.pos - b2.pos)
+            )
+            v2 = b2.velocity - (
+                ((2 * b1.mass) / (b1.mass + b2.mass))
+                * (
+                    ((b2.velocity - b1.velocity).dot(b2.pos - b1.pos))
+                    / (b2.pos - b1.pos).length_squared()
+                )
+                * (b2.pos - b1.pos)
+            )
+
+            b1.velocity = v1
+            b2.velocity = v2
+
+    def update_collision(self, ball: Ball) -> None:
+        if (
+            ball.pos.x + ball.radius >= self.model.size.width
+            and ball.velocity.x > 0
+            or ball.pos.x - ball.radius <= 0
+            and ball.velocity.x < 0
+        ):
+            ball.velocity.reflect_ip(Vector2(1, 0))
+        if (
+            ball.pos.y + ball.radius >= self.model.size.height
+            and ball.velocity.y > 0
+            or ball.pos.y - ball.radius <= 0
+            and ball.velocity.y < 0
+        ):
+            ball.velocity.reflect_ip(Vector2(0, 1))
+
+        for wall in self.walls:
+            response = collision.Response()
+            wall_collider = collision.Poly(
+                collision.Vector(wall.rect.x, wall.rect.y),
+                [collision.Vector(p[0], p[1]) for p in wall.rel_points],
+            )
+            ball_collider = collision.Circle(
+                collision.Vector(ball.pos.x, ball.pos.y),
+                ball.radius,
+            )
+
+            if not collision.test_poly_circle(
+                wall_collider,
+                ball_collider,
+                response,
+            ):
+                continue
+            edge = Vector2(response.overlap_v.x, response.overlap_v.y)
+            if edge.length() != 0:
+                ball.velocity.reflect_ip(edge)
+
+    def update_ball(self, ball: Ball, time: float) -> None:
+        friction = self.model.friction
+
+        for zone in self.zones:
+            if pg.sprite.collide_mask(zone, ball):
                 if isinstance(zone, FrictionZone):
                     friction = zone.friction
                 if isinstance(zone, DeadZone):
@@ -79,80 +144,20 @@ class Course:
                     ball.pos = Vector2(self.model.start.point)
                     ball.update()
 
-            for collided_ball in self.balls:
-                if (
-                    ball is collided_ball
-                    or (collided_ball.pos - ball.pos).length_squared() == 0
-                    or not pg.sprite.collide_circle(collided_ball, ball)
-                ):
-                    continue
+        if ball.velocity.length() != 0:
+            acceleration = -ball.velocity.normalize() * friction * 9.8
+            ball.pos += ball.velocity * time + (acceleration * time**2) / 2
+            ball.velocity += acceleration
 
-                b1 = collided_ball
-                b2 = ball
+            if ball.velocity.length() - acceleration.length() <= 0:
+                ball.velocity = Vector2()
 
-                v1 = b1.velocity - (
-                    ((2 * b2.mass) / (b1.mass + b2.mass))
-                    * (
-                        ((b1.velocity - b2.velocity).dot(b1.pos - b2.pos))
-                        / (b1.pos - b2.pos).length_squared()
-                    )
-                    * (b1.pos - b2.pos)
-                )
-                v2 = b2.velocity - (
-                    ((2 * b1.mass) / (b1.mass + b2.mass))
-                    * (
-                        ((b2.velocity - b1.velocity).dot(b2.pos - b1.pos))
-                        / (b2.pos - b1.pos).length_squared()
-                    )
-                    * (b2.pos - b1.pos)
-                )
+            ball.update()
 
-                b1.velocity = v1
-                b2.velocity = v2
+    def update(self) -> None:
+        time = self.clock.tick(self.fps) / 1000
 
-            if (
-                ball.pos.x + ball.radius >= self.model.size.width
-                and ball.velocity.x > 0
-                or ball.pos.x - ball.radius <= 0
-                and ball.velocity.x < 0
-            ):
-                ball.velocity.reflect_ip(Vector2(1, 0))
-            if (
-                ball.pos.y + ball.radius >= self.model.size.height
-                and ball.velocity.y > 0
-                or ball.pos.y - ball.radius <= 0
-                and ball.velocity.y < 0
-            ):
-                ball.velocity.reflect_ip(Vector2(0, 1))
-
-            for wall in self.walls:
-                response = collision.Response()
-                wall_collider = collision.Poly(
-                    collision.Vector(wall.rect.x, wall.rect.y),
-                    [collision.Vector(p[0], p[1]) for p in wall.rel_points],
-                )
-                ball_collider = collision.Circle(
-                    collision.Vector(ball.pos.x, ball.pos.y),
-                    ball.radius,
-                )
-
-                if not collision.test_poly_circle(
-                    wall_collider,
-                    ball_collider,
-                    response,
-                ):
-                    continue
-
-                ball.velocity.reflect_ip(
-                    Vector2(response.overlap_v.x, response.overlap_v.y)
-                )
-
-            if ball.velocity.length() != 0:
-                acceleration = -ball.velocity.normalize() * friction * 9.8
-                ball.pos += ball.velocity * time + (acceleration * time**2) / 2
-                ball.velocity += acceleration
-
-                if ball.velocity.length() - acceleration.length() <= 0:
-                    ball.velocity = Vector2()
-
-                ball.update()
+        for ball in self.balls:
+            self.update_impulse(ball)
+            self.update_collision(ball)
+            self.update_ball(ball, time)
