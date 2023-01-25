66f457c2631205171f32622be7d3dd0f79b4989d
arslee07 <mail@arslee.me>
2023-01-25 19:31:14 +0900

    link level to user

--- server/database.py
+++ server/database.py
@@ -1,6 +1,12 @@
 from settings import settings
-from sqlalchemy import JSON, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy import JSON, ForeignKey, create_engine
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+    sessionmaker,
+)
 
 engine = create_engine(settings.database_url)
 session = sessionmaker(engine)
@@ -21,5 +27,8 @@ class User(Base):
 class Level(Base):
     __tablename__ = "levels"
     id: Mapped[int] = mapped_column(primary_key=True)
+    owner_id: Mapped[int] = mapped_column(ForeignKey(User.id, ondelete="CASCADE"))
     name: Mapped[str]
     course: Mapped[dict] = mapped_column(JSON)
+
+    owner: Mapped[User] = relationship(User)
