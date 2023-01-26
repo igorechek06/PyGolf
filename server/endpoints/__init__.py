e300a4563908829bdf0ff38be0c1901ad3e56435
arslee07 <mail@arslee.me>
2023-01-26 22:59:29 +0900

    забыл

--- server/endpoints/__init__.py
+++ server/endpoints/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from . import level, user
+from . import level, user, score
 
 router = APIRouter()
 router.include_router(user.router)
 router.include_router(level.router)
+router.include_router(score.router)
